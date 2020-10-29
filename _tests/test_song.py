bd1907f7bbe269d7e3e2f7389de68fa7d897509c
Thibault Lebrun <thibaultlebrun@live.fr>
2020-10-29 20:58:35 +0100

    backup

---
+++ _tests/test_song.py
@@ -0,0 +1,19 @@
+import pytest
+import sys
+
+sys.path.insert(0, " C:\ProgramData\Ableton\Live 10 Suite\Resources\MIDI Remote Scripts")
+from ClyphX_Pro.clyphx_pro.user_actions.lom.Song import Song
+# from ..lom.Song import Song
+
+
+class AbletonSong:
+    @property
+    def tracks(self):
+        return []
+
+@pytest.fixture
+def empty_song():
+    return Song(AbletonSong())
+
+def test_default_song(empty_song):
+    assert len(empty_song.tracks) == 0
