faf638bc5b317368f460ea56c8816034468608bc
Thibault <thibaultlebrun@live.fr>
2021-11-13 10:46:46 +0100

    'backup'

--- enums/vocal_command/ActionEnum.py
+++ enums/vocal_command/ActionEnum.py
@@ -4,6 +4,7 @@ from protocol0.enums.AbstractEnum import AbstractEnum
 class ActionEnum(AbstractEnum):
     PLAY = "PLAY"  # play set
     PAUSE = "PAUSE"  # pause set
+    HOUSE = "HOUSE"  # pause set
     STOP = "STOP"  # stop set
     FOLD = "FOLD"  # fold set
     NEXT = "NEXT"  # repeat previous command
