ce7dd4171f65a9580ac26eaf33bef17f4e22cf90
Thibault <thibaultlebrun@live.fr>
2021-12-21 23:54:20 +0100

    remove jupyter

--- jupyter/.ipynb_checkpoints/startup-checkpoint.py
+++
@@ -1,6 +0,0 @@
-from functools import partial
-from a_protocol_0.sequence.Sequence import Sequence
-from __future__ import print_function
-
-from a_protocol_0 import Protocol0, EmptyModule
-p0 = Protocol0(EmptyModule(is_false=False), init_song=False)
