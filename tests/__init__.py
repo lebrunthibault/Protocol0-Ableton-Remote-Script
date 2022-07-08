a7ac1ebe179bb71e20176381f24cb5b5ce61f500
Thibault <thibaultlebrun@live.fr>
2022-07-08 18:44:40 +0200

    fix sys.path

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,8 @@
+import os
 import sys
+from os.path import dirname
 
+sys.path.insert(0, dirname(os.path.realpath(__file__)))
 from protocol0.tests.domain.fixtures.p0 import monkey_patch_static
 from protocol0.tests.infra.scheduler.TickSchedulerTest import TickSchedulerTest
 
