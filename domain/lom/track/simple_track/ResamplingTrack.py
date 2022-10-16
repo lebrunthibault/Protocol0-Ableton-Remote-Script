1561a53d7884756faf1b405c34b0d00291bb658e
Thibault <thibaultlebrun@live.fr>
2022-10-16 17:59:02 +0200

    'backup'

--- domain/lom/track/simple_track/ResamplingTrack.py
+++ domain/lom/track/simple_track/ResamplingTrack.py
@@ -1,7 +1,6 @@
 from typing import Any
 
 from protocol0.domain.lom.track.simple_track.SimpleTrack import SimpleTrack
-from protocol0.shared.logging.Logger import Logger
 
 
 class ResamplingTrack(SimpleTrack):
@@ -9,7 +8,6 @@ class ResamplingTrack(SimpleTrack):
 
     def stop(self, *a, **k):
         # type: (Any, Any) -> None
-        Logger.dev(self.is_recording)
         if self.is_recording:
             return
 
