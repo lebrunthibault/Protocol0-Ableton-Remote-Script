173f6df567dcf56480069c430c586818e626d5fd
Thibault <thibaultlebrun@live.fr>
2022-11-08 21:34:46 +0100

    'Backup'

--- domain/lom/track/simple_track/InstrumentBusTrack.py
+++ domain/lom/track/simple_track/InstrumentBusTrack.py
@@ -1,9 +1,11 @@
 from typing import Any, Optional
 
 from protocol0.domain.lom.clip_slot.AudioClipSlot import AudioClipSlot
+from protocol0.domain.lom.instrument.preset.SampleSelectedEvent import SampleSelectedEvent
 from protocol0.domain.lom.track.simple_track.SimpleAudioTrack import SimpleAudioTrack
 from protocol0.domain.shared.backend.Backend import Backend
 from protocol0.domain.shared.errors.Protocol0Error import Protocol0Error
+from protocol0.domain.shared.event.DomainEventBus import DomainEventBus
 from protocol0.domain.shared.scheduler.Scheduler import Scheduler
 from protocol0.shared.logging.Logger import Logger
 
@@ -40,7 +42,13 @@ class InstrumentBusTrack(SimpleAudioTrack):
         super(InstrumentBusTrack, self).on_scenes_change()
 
         if has_template_dummy_clip and self.template_dummy_clip_slot is None:
-            Backend.client().show_warning("Template dummy clip removed")
+            self._load_dummy_clip()
 
         for clip in self.clips[1:]:
             Scheduler.defer(clip.delete)
+
+    def _load_dummy_clip(self):
+        # type: () -> None
+        self.select_clip_slot(self.clip_slots[0]._clip_slot)
+        DomainEventBus.emit(SampleSelectedEvent("dummy clip.wav"))
+        Backend.client().show_info("Loaded dummy clip")
