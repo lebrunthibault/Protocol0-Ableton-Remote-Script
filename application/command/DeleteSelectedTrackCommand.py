1188f73ef6f59a9dafb20ea6abfd8473ff99f75a
Thibault <thibaultlebrun@live.fr>
2022-11-29 14:11:40 +0100

    'Backup'

--- application/command/DeleteSelectedTrackCommand.py
+++ application/command/DeleteSelectedTrackCommand.py
@@ -1,5 +1,10 @@
+from typing import Optional
+
 from protocol0.application.command.SerializableCommand import SerializableCommand
 
 
 class DeleteSelectedTrackCommand(SerializableCommand):
-    pass
+    def __init__(self, track_name=None):
+        # type:  (Optional[str]) -> None
+        super(DeleteSelectedTrackCommand, self).__init__()
+        self.track_name = track_name
--- application/command_handler/DeleteSelectedTrackCommandHandler.py
+++ application/command_handler/DeleteSelectedTrackCommandHandler.py
@@ -1,9 +1,12 @@
 from protocol0.application.command.DeleteSelectedTrackCommand import DeleteSelectedTrackCommand
 from protocol0.application.command_handler.CommandHandlerInterface import CommandHandlerInterface
+from protocol0.domain.shared.errors.Protocol0Warning import Protocol0Warning
 from protocol0.shared.SongFacade import SongFacade
 
 
 class DeleteSelectedTrackCommandHandler(CommandHandlerInterface):
-    def handle(self, _):
+    def handle(self, command):
         # type: (DeleteSelectedTrackCommand) -> None
+        if command.track_name is not None and command.track_name != SongFacade.selected_track().name:
+            raise Protocol0Warning("Cannot delete non selected '%s' track" % command.track_name)
         SongFacade.selected_track().delete()
