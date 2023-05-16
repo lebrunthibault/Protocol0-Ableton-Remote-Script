90eaf3ab906ed7528654310c386212bcaa4dde53
Thibault <thibaultlebrun@live.fr>
2023-05-16 19:21:51 +0200

    'Backup'

--- domain/audit/stats/TrackStats.py
+++ domain/audit/stats/TrackStats.py
@@ -2,7 +2,7 @@ import collections
 
 from typing import Dict
 
-from protocol0.domain.lom.track.group_track.AbstractGroupTrack import AbstractGroupTrack
+from protocol0.domain.lom.track.group_track.NormalGroupTrack import NormalGroupTrack
 from protocol0.domain.lom.track.simple_track.midi.special.UsamoTrack import UsamoTrack
 from protocol0.shared.Song import Song
 
@@ -10,7 +10,7 @@ from protocol0.shared.Song import Song
 class TrackStats(object):
     def __init__(self):
         # type: () -> None
-        excluded_track_classes = (AbstractGroupTrack, UsamoTrack)
+        excluded_track_classes = (NormalGroupTrack, UsamoTrack)
         tracks = [t for t in Song.abstract_tracks() if not isinstance(t, excluded_track_classes)]
         tracks_sorted = sorted(tracks, key=lambda track: track.load_time)
         self._slow_tracks = [t for t in list(reversed(tracks_sorted))[0:10] if t.load_time]
